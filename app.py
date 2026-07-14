165bde06ac2c67858f01aae8f6ebc8f6259e3841
Utpal.Das6 <utpaldas.6osm@gmail.com>
2026-07-14 16:40:18 +0530

    Remove Google OAuth, back to single-user app
    Drops login/logout/OAuth callback routes and the authlib dependency.
    User is now an implicit singleton row instead of a Google-linked
    account, so tasks/habits/coins work without signing in.

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import os
 from datetime import date, timedelta
-from functools import wraps
 
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, redirect, url_for, session, g
+from flask import Flask, render_template, request, redirect, url_for, g
 from flask_sqlalchemy import SQLAlchemy
-from authlib.integrations.flask_client import OAuth
 
 load_dotenv()
 
@@ -19,15 +17,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-oauth = OAuth(app)
-oauth.register(
-    name='google',
-    client_id=os.environ.get('GOOGLE_CLIENT_ID', ''),
-    client_secret=os.environ.get('GOOGLE_CLIENT_SECRET', ''),
-    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    client_kwargs={'scope': 'openid email profile'},
-)
-
 TASK_COINS = 5
 HABIT_COINS = 10
 MILESTONE_EVERY = 7      # days
@@ -36,10 +25,6 @@ MILESTONE_BONUS = 50     # coins
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    google_sub = db.Column(db.String(64), unique=True, nullable=False)
-    email = db.Column(db.String(200))
-    name = db.Column(db.String(200))
-    picture = db.Column(db.String(500))
     coins = db.Column(db.Integer, default=0)
 
 
@@ -70,27 +55,20 @@ def next_streak(current_streak, last_done, today):
 
 @app.before_request
 def load_user():
-    g.user = User.query.get(session['user_id']) if 'user_id' in session else None
-
-
-def login_required(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        if g.user is None:
-            return redirect(url_for('home'))
-        return f(*args, **kwargs)
-    return wrapper
+    user = User.query.first()
+    if user is None:
+        user = User(coins=0)
+        db.session.add(user)
+        db.session.commit()
+    g.user = user
 
 
 @app.route("/")
 def home():
-    if g.user is None:
-        return render_template("base.html", logged_in=False)
     todo_list = Todo.query.filter_by(user_id=g.user.id).all()
     habits = Habit.query.filter_by(user_id=g.user.id).all()
     return render_template(
         "base.html",
-        logged_in=True,
         todo_list=todo_list,
         habits=habits,
         today=date.today(),
@@ -98,38 +76,7 @@ def home():
     )
 
 
-@app.route('/login')
-def login():
-    redirect_uri = url_for('auth_callback', _external=True)
-    return oauth.google.authorize_redirect(redirect_uri)
-
-
-@app.route('/auth/callback')
-def auth_callback():
-    token = oauth.google.authorize_access_token()
-    info = token['userinfo']
-    user = User.query.filter_by(google_sub=info['sub']).first()
-    if user is None:
-        user = User(
-            google_sub=info['sub'],
-            email=info.get('email'),
-            name=info.get('name'),
-            picture=info.get('picture'),
-        )
-        db.session.add(user)
-        db.session.commit()
-    session['user_id'] = user.id
-    return redirect(url_for('home'))
-
-
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('home'))
-
-
 @app.route("/add", methods=["POST"])
-@login_required
 def add():
     title = request.form.get("title")
     db.session.add(Todo(title=title, complete=False, user_id=g.user.id))
@@ -138,7 +85,6 @@ def add():
 
 
 @app.route("/update/<int:todo_id>")
-@login_required
 def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id, user_id=g.user.id).first_or_404()
     todo.complete = not todo.complete
@@ -151,7 +97,6 @@ def update(todo_id):
 
 
 @app.route("/delete/<int:todo_id>")
-@login_required
 def delete(todo_id):
     todo = Todo.query.filter_by(id=todo_id, user_id=g.user.id).first_or_404()
     db.session.delete(todo)
@@ -160,7 +105,6 @@ def delete(todo_id):
 
 
 @app.route("/habit/add", methods=["POST"])
-@login_required
 def habit_add():
     name = request.form.get("name")
     db.session.add(Habit(name=name, streak=0, best_streak=0, last_done=None, user_id=g.user.id))
@@ -169,7 +113,6 @@ def habit_add():
 
 
 @app.route("/habit/checkin/<int:habit_id>")
-@login_required
 def habit_checkin(habit_id):
     habit = Habit.query.filter_by(id=habit_id, user_id=g.user.id).first_or_404()
     today = date.today()
@@ -185,7 +128,6 @@ def habit_checkin(habit_id):
 
 
 @app.route("/habit/delete/<int:habit_id>")
-@login_required
 def habit_delete(habit_id):
     habit = Habit.query.filter_by(id=habit_id, user_id=g.user.id).first_or_404()
     db.session.delete(habit)
@@ -193,7 +135,8 @@ def habit_delete(habit_id):
     return redirect(url_for("home"))
 
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
